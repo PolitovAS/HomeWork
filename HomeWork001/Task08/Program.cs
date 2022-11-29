bfcc1891f6a068f53967c279895b2694bee8ebe0
Alexandr Politov <razreztv@gmail.com>
2022-11-30 02:58:10 +0500

    Add solution fourth task(08)

--- HomeWork001/Task08/Program.cs
+++ HomeWork001/Task08/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.WriteLine("Введите число: ");
+string nfirst = Console.ReadLine() ?? "";
+int n = Convert.ToInt32(nfirst);
+int count = 1;
+
+if (n <= count)
+{
+    Console.Write("Число меньше необходимого минимального диапазона");
+}
+while (count < n+1)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + ", ");
+    }
+    count = count + 1;
+}
