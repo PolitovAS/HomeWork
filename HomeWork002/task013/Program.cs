1e4b065beeebfb938f993e1053e15724e1b51fcf
Alexandr Politov <razreztv@gmail.com>
2022-12-03 02:48:07 +0500

    Add HomeWork002

---
+++ HomeWork002/task013/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+    Console.WriteLine("Третьей цифры нет!");
+else
+{
+    string str = number.ToString();
+    Console.WriteLine($"{str[2]}");
+}
