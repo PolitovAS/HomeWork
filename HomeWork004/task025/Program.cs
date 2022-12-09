805d7cdfd55a63a1b94ceb443cbd1a23b9062a8d
Alexandr Politov <razreztv@gmail.com>
2022-12-10 00:32:43 +0500

    Add HomeWork004 and update README

---
+++ HomeWork004/task025/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+// 1-й способ решения, без использования метода на проверку корректности вводимых чисел
+/*Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+for (int i = 1; i <= b; i++)
+{
+    result = result * a;
+}
+
+Console.WriteLine($"Результат возведения числа {a} в степень {b} равен {result}");*/
+
+// 2-й способ решения, с использованием методов
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Введите число.");
+        }
+    }
+
+    return result;
+}
+
+int GetPow(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+
+    return result;
+}
+
+int a = GetNumber("Введите первое число: ");
+int b = GetNumber("Введите второе число: ");
+int pow = GetPow(a, b);
+
+Console.WriteLine($"Результат возведения числа {a} в степень {b} равен {pow}");
---
+++ HomeWork004/task027/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Введите число больше 0.");
+        }
+    }
+
+    return result;
+}
+
+int GetSumm(int number)
+{
+    int result = 0;
+    for (int i = number; i >= 0; i--)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = GetNumber("Введите число: ");
+int summ = GetSumm(number);
+Console.WriteLine(($"Сумма цифр в числе {number} = {summ}"));
