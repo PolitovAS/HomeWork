5e001175633132e74f2389a82b0339caeb1943f7
Alexandr Politov <razreztv@gmail.com>
2022-12-25 01:32:18 +0500

    fix task 062

--- HomeWork008/task062/Program.cs
+++ HomeWork008/task062/Program.cs
@@ -6,27 +6,32 @@
 11 16 15 06
 10 09 08 07
 */
-int line = 4;
-int[,] matrix = new int[line, line];
+
+Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine();
+int[,] matrix = new int[rows, columns];
 int count = 1;
-for (int x = 0; x < (line * line); x++)
+for (int x = 0; IsNeed(x, rows, columns); x++)
 {
-    for (int i = x; i < line - x; i++)
+    for (int i = x; i < columns - x; i++)
     {
-        matrix[x,i] = count;
+        matrix[x, i] = count;
         count++;
     }
-    for (int i = x + 1; i < line - x; i++)
+    for (int i = x + 1; i < rows - x; i++)
     {
-        matrix[i, line - 1 - x] = count;
+        matrix[i, columns - 1 - x] = count;
         count++;
     }
-    for (int i = line - 2 - x; i >= x; i--)
+    for (int i = columns - 2 - x; i >= x; i--)
     {
-        matrix[line - 1 - x, i] = count;
+        matrix[rows - 1 - x, i] = count;
         count++;
     }
-    for (int i = line - 2 - x; i > x; i--)
+    for (int i = rows - 2 - x; i > x; i--)
     {
         matrix[i, x] = count;
         count++;
@@ -36,8 +41,25 @@ for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        Console.Write(matrix[i,j] + "\t");
+        Console.Write(matrix[i, j] + "\t");
     }
-    System.Console.WriteLine("\n");
+    Console.WriteLine("\n");
 }
-System.Console.WriteLine();
+Console.WriteLine();
+
+
+bool IsNeed(int x, int rows, int columns)
+{
+    if(rows < columns)
+    {
+        if(rows % 2 == 0)
+            return x < rows / 2;
+        else
+            return x <= rows / 2;
+    }
+    
+    if (columns % 2 == 0)
+        return x < columns / 2;
+    else
+        return x <= columns / 2;
+}
